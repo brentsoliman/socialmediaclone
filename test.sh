57b1de0ef099bb8e2b5aa137f5e6428b1f4706b9
Brent Soliman <shebna@Brents-Computer.local>
2023-02-10 19:58:17 -0700

    removed the ignored files that was commited earlier

--- test.sh
+++
@@ -1,7 +0,0 @@
-docker container run -d --rm -p 5003:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres --name test_preloaded_db preloaded_db:latest
-sleep 1
-source env/bin/activate
-pytest -v
-#sleep 1
-deactivate
-docker stop test_preloaded_db
